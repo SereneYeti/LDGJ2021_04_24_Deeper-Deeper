0988527427e894daa5603fb580e6fa38e6572120
James D Vieyra <james@vieyra.co.za>
2021-04-24 13:57:07 +0200

    Got 1st person Movement & Looking Script working
    Implemented a basic first Person Movement with a character controller, a simple mouseLook script on the camera was also implemented

---
+++ Assets/Scripts/Player/MouseLook.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float mouseSensitivity = 100f;
+    public Transform playerBody;
+
+    float xRotatation = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float inputX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float inputY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotatation -= inputY;
+        xRotatation = Mathf.Clamp(xRotatation, -45f, 45f);
+
+        transform.localRotation = Quaternion.Euler(xRotatation, 0, 0);
+        playerBody.Rotate(Vector3.up * inputX);
+    }
+}
