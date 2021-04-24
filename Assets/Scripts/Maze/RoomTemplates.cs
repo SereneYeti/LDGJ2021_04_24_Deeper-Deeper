0746994b0a71a7ef39671d66bd09d47e00f13232
James D Vieyra <james@vieyra.co.za>
2021-04-24 17:41:15 +0200

    Simple updates

---
+++ Assets/Scripts/Maze/RoomTemplates.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomTemplates : MonoBehaviour
+{
+    public GameObject[] TopRooms;
+    public GameObject[] BottomRooms;
+    public GameObject[] RightRooms;
+    public GameObject[] LeftRooms;}
