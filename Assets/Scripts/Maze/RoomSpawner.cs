3e30d3f0435840a468f9f99cea0936b32d1f2671
James D Vieyra <james@vieyra.co.za>
2021-04-24 18:47:23 +0200

    roomSpawner changes

--- Assets/Scripts/Maze/RoomSpawner.cs
+++ Assets/Scripts/Maze/RoomSpawner.cs
@@ -4,43 +4,67 @@ using UnityEngine;
 
 public class RoomSpawner : MonoBehaviour
 {
-    public int openingDir;
+    public int openingDir;    
     //1 -> need bottom door
     //2 -> need top door
     //3 -> need left door
     //4 -> need right door 
 
     private RoomTemplates templates;
+    private int rnd;
+    private bool spawned = false;
     void Start()
     {
         templates = GameObject.FindObjectOfType<RoomTemplates>();
+        Invoke("Spawn", 0.1f);
     }
 
     // Update is called once per frame
-    void Update()
+    void Spawn()
     {
-        switch(openingDir)
+        if(!spawned)
         {
-            case 1:
-                {
-                    //1 -> need bottom door
-                    break;
-                }
-            case 2:
-                {
-                    //2 -> need top door
-                    break;
-                }
-            case 3:
-                {
-                    //3 -> need left door
-                    break;
-                }
-            case 4:
-                {
-                    //4 -> need right door
-                    break;
-                }
+            switch (openingDir)
+            {
+                case 1:
+                    {
+                        //1 -> need bottom door
+                        rnd = Random.Range(0, templates.BottomRooms.Length);
+                        Instantiate(templates.BottomRooms[rnd], transform.position, templates.BottomRooms[rnd].transform.rotation);
+                        break;
+                    }
+                case 2:
+                    {
+                        //2 -> need top door
+                        rnd = Random.Range(0, templates.TopRooms.Length);
+                        Instantiate(templates.TopRooms[rnd], transform.position, templates.TopRooms[rnd].transform.rotation);
+                        break;
+                    }
+                case 3:
+                    {
+                        //3 -> need left door
+                        rnd = Random.Range(0, templates.LeftRooms.Length);
+                        Instantiate(templates.LeftRooms[rnd], transform.position, templates.LeftRooms[rnd].transform.rotation);
+                        break;
+                    }
+                case 4:
+                    {
+                        //4 -> need right door
+                        rnd = Random.Range(0, templates.RightRooms.Length);
+                        Instantiate(templates.RightRooms[rnd], transform.position, templates.RightRooms[rnd].transform.rotation);
+                        break;
+                    }
+            }
+            spawned = true;
+        }      
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("SpawnPoint") && other.GetComponent<RoomSpawner>().spawned == true)
+        {
+            Destroy(gameObject);
         }
     }
 }
