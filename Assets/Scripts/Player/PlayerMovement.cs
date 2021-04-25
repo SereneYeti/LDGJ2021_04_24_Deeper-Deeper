1e0308975a3a9f535aadf0131ca0c640302882b9
perayepat <56199797+perayepat@users.noreply.github.com>
2021-04-25 10:37:06 +0200

    Movement Changes

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -7,11 +7,24 @@ public class PlayerMovement : MonoBehaviour
 {
     public CharacterController controller;
     public float speed = 5f;
+    public float walkspeed = 5;
+    public float sprintSpeed = 8f;
+    public float sprintingSpeed = 12f;
     public float gravity = -9.81f;
     public float jumpHeight = 3f;
-    public float _dashTime = 0.30f;    
+    public float _dashTime = 0.30f;
     //public float _dashSpeed = 60f;
     //public float dashDistance = 10f;
+
+
+    //movement 
+    KeyCode codes;
+   public  float startTime;
+    //Jump setting 
+    
+    public float fallMultiplier = 2.5f;
+    public float lowJumpMultiplier = 2f;
+    public float jVelocity;
 
     public Transform groundCheck;
     public float groundDistance = 0.4f;
@@ -20,7 +33,13 @@ public class PlayerMovement : MonoBehaviour
     Vector3 velocity;
     bool isGrounded;
     //Vector3 lastMoveDir;
-    
+
+
+
+    private void Start()
+    {
+        
+    }
     // Update is called once per frame
     void Update()
     {
@@ -36,14 +55,63 @@ public class PlayerMovement : MonoBehaviour
 
         Vector3 move = transform.right * x + transform.forward * z;
 
+        //walking & Sprinting 
         controller.Move(move * speed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            Debug.Log("Sprinting");
+            speed = sprintSpeed;
+        }
+        else
+        {
+            speed = walkspeed;
+        }
 
-        if(Input.GetButtonDown("Jump") && isGrounded)
+
+        switch (codes)
+        {
+           
+            case KeyCode.W:
+                startTime = Time.time;
+                if (startTime < 2f)
+                {
+                    speed = sprintingSpeed;
+                }
+                if (Input.GetKeyUp(KeyCode.W))
+                {
+                    float timePressed = Time.time - startTime;
+                }
+                break;
+            case KeyCode.S:
+                startTime = Time.time;
+                break;
+            case KeyCode.D:
+                startTime = Time.time;
+                break;
+            case KeyCode.A:
+                startTime = Time.time;
+                break;
+        }
+        //Adjusitng the fall multiplier for better jumps 
+
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
         {
             velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
         }
 
-        velocity.y += gravity * 2 * Time.deltaTime;
+      
+        if (velocity.y < 0)
+        {
+            velocity += Vector3.up * Physics.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        }
+        else if (velocity.y > 0 && !Input.GetButton("Jump"))
+        {
+
+            velocity += Vector3.up * Physics.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+        }
+
+     velocity.y += gravity * 2 * Time.deltaTime;
 
         controller.Move(velocity * Time.deltaTime);
 
