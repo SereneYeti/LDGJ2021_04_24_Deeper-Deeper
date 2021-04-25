d5e1c19a0a607c046aca866593a3b7dec51f0eab
James D Vieyra <james@vieyra.co.za>
2021-04-25 11:27:52 +0200

    Changed triggers to colliders

--- Assets/Scripts/Maze/Destroy_Create.cs
+++ Assets/Scripts/Maze/Destroy_Create.cs
@@ -7,18 +7,29 @@ public class Destroy_Create : MonoBehaviour
     public GameObject door;
     private void Start()
     {
-        LeanTween.moveLocalY(gameObject, 100f, 1f).setEaseOutQuad();
+        LeanTween.moveLocalY(gameObject, 2f, 1f).setEaseOutQuad();
         //Destroy(gameObject, 4f);
     }
 
     private void Update()
     {
-        LeanTween.moveLocalY(gameObject, 1f, 2f).setEaseInOutQuad();
+        //LeanTween.moveLocalY(gameObject, 1f, 2f).setEaseInOutQuad();
   
     }
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("is anyone out there!>!!?1");
+        //Destroy(other.gameObject);
+        //if (other.CompareTag("Rooms"))
+        //{
+
+        //    //Instantiate(door, this.transform.position, Quaternion.identity);
+        //}
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("is anyone out there!>!!?2");
         //Destroy(other.gameObject);
         //if (other.CompareTag("Rooms"))
         //{
