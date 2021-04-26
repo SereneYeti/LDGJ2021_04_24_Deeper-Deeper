709d7f232ed58a5f603fdf59bd471c7e4fa00890
James D Vieyra <james@vieyra.co.za>
2021-04-26 23:24:00 +0200

    minor changes

--- Assets/Scripts/Player/MouseLook.cs
+++ Assets/Scripts/Player/MouseLook.cs
@@ -15,7 +15,7 @@ public class MouseLook : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
         float inputX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float inputY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
@@ -23,7 +23,7 @@ public class MouseLook : MonoBehaviour
         xRotatation -= inputY;
         xRotatation = Mathf.Clamp(xRotatation, -90f, 45f);
 
-        transform.localRotation = Quaternion.Euler(xRotatation*2, 0, 0);
-        playerBody.Rotate(Vector3.up * inputX*Time.deltaTime *50);
+        transform.localRotation = Quaternion.Euler(xRotatation * 2, 0, 0);
+        playerBody.Rotate(Vector3.up * inputX * Time.deltaTime * 50);
     }
 }
